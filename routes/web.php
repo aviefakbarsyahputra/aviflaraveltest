3e18ceeb91b2910f48295474d048f7eed2099368
Avief Akbarsyah Putra <59563160+aviefakbarsyahputra@users.noreply.github.com>
2023-03-09 13:39:17 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-});
+})->middleware('guest');
 
 Route::get('/login', [AuthController::class, 'login'])->name('login')->middleware('guest');
 Route::post('/login', [AuthController::class, 'authentication'])->middleware('guest');
